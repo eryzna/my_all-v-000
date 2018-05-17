0417c7a3d9315910816839cdaf6b2b8f170291c8
Elana Ryznar <eryzna@gmail.com>
2018-05-17 01:27:55 +0000

    automatically backed up by learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -8,4 +8,9 @@ def my_all?(collection)
     i = i + 1   #i += 1 does the same thing. Use this if it's easier for you.
   end
 
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
